94a6bf81f8f5b851b9ee86763f5573d1d76c3d24
James W. Goedert <JamesWGoedert@Jamess-MacBook-Pro.local>
2018-04-19 00:59:30 -0500

    Boiler plate for Angular setup

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JamesWGoedert/Desktop/G3/Goedert_Family_Photos/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
